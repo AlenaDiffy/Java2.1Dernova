4ffb7da5d30867b9ea274cdc068d526fc42f1ce9
AlenaDiffy <alenadernova@gmail.com>
2021-12-28 00:30:02 +0300

    ICOM

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 2000; // переменная для стоимости билета
+        int milesRewardPrice = 20; // переменная стоимости одной мили
+        System.out.println("Вам начисленно " + ticketPrice / milesRewardPrice + " бонусных миль");
+
+
+    }
+}
